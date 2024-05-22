9708dc1e97010cafbf81817d2c954e80b64f77f7
Julius Hell <jhell2306@googlemail.com>
2024-05-22 20:50:56 +0200

    Added mileage chart

--- src/lib/components/MileageCard.svelte
+++ src/lib/components/MileageCard.svelte
@@ -2,8 +2,44 @@
     import * as Card from "$lib/components/ui/card";
     import * as Table from "$lib/components/ui/table";
     import type {MileageItem} from "$lib/types/mileageItem";
-
+    import 'chart.js/auto';
+    import { Line } from "svelte-chartjs";
+    import 'chartjs-adapter-date-fns';
     export let mileageItems = undefined as MileageItem[] | undefined;
+
+    $: chartData = {
+        labels: (mileageItems ?? []).toReversed().map(item => item.created),
+        datasets: [
+            {
+                label: "Car mileage",
+                data: (mileageItems ?? []).toReversed().map(item => item.mileage),
+                borderColor: "hsl(346.8,77.2%,49.8%)",
+                backgroundColor: "hsla(346.8,77.2%,49.8%, 0.075)",
+                tension: 0.15,
+                fill: true,
+            }
+        ]
+    };
+
+    $: options = {
+        responsive: true,
+        plugins: {
+            legend: {
+                display: false,
+            },
+        },
+        scales: {
+            x: {
+                type: "timeseries",
+                time: {
+                    unit: "date",
+                    displayFormats: {
+                        date: "dd.MM.yyyy HH:mm"
+                    }
+                },
+            },
+        },
+    }
 </script>
 
 <Card.Root>
@@ -29,5 +65,8 @@
                 {/if}
             </Table.Body>
         </Table.Root>
+        {#if chartData.labels.length > 0}
+        <Line data={chartData} {options} />
+        {/if}
     </Card.Content>
 </Card.Root>
