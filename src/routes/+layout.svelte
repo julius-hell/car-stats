db4b1b271cedab8d29a0f3c3955fca11ca6c9c98
Julius Hell <jhell2306@googlemail.com>
2024-05-25 18:49:56 +0200

    Added manifest

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,13 @@
 <script>
     import "../app.css";
+    import { pwaInfo } from 'virtual:pwa-info';
+
+    $: webManifestLink = pwaInfo ? pwaInfo.webManifest.linkTag : ''
 </script>
+
+<svelte:head>
+    {@html  webManifestLink}
+</svelte:head>
 <nav class="border-gray-200">
     <div class="container flex flex-wrap items-center justify-between mx-auto py-4">
         <a href="/">
